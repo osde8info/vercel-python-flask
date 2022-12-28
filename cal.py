6072968b3ec77ceeb7d2701ec6fcea0e79a74e4c
Clive D <clive@github.com>
2022-12-28 14:43:56 +0000

    add cal

---
+++ cal.py
@@ -0,0 +1,9 @@
+import datetime
+
+x = datetime.datetime.now()
+print(x)
+
+import calendar
+year = 2023 # // input("Enter year to view: ")
+month = 1 # // input("Enter month to view: ")
+print(calendar.month(year,month))
--- index.py
+++ index.py
@@ -1,11 +1,16 @@
+import datetime
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    today = datetime.datetime.now()
+
+    return 'Hello, World!' + today
 
 @app.route('/about')
 def about():
     return 'About'
+    
